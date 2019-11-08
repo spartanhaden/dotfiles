0cb5be283393644e0056a4c4f4984e744f06bd0d
Haden Wasserbaech <spartanhaden@gmail.com>
2019-11-08 04:04:53 -0500

    Adds packages I commonly use on linux

---
+++ install_packages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Setup Chrome PPA
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt update
+sudo apt install -y \
+  sl \
+  tree \
+  nano \
+  htop \
+  net-tools \
+  openssh-server \
+  sshfs \
+  gnome-tweaks \
+  dconf-editor \
+  git \
+  xclip \
+  arc-theme \
+  papirus-icon-theme \
+  google-chrome-stable \
+  vlc \
+  wireshark \
+  nmap \
+  ffmpeg \
+  handbrake \
+  iotop \
+  expect \
+  tig \
+  zsh
+
+sudo snap install \
+  discord \
+  google-play-music-desktop-player
+
+sudo snap install --classic slack
+
+sudo snap install --classic code
