8e4f45781ebf6a0e16e4de5c232da9cab72e5006
Haden Wasserbaech <spartanhaden@gmail.com>
2019-11-08 04:24:24 -0500

    Adds VSCode package install

---
+++ vs_code_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+code --list-extensions --show-versions
+
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+
+# ROS
+code --install-extension ms-iot.vscode-ros
+
+# Python
+code --install-extension ms-python.python
+
+# C++
+code --install-extension ms-vscode.cpptools
+
+# Working in containers
+code --install-extension ms-vscode-remote.remote-containers
