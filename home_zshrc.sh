59fe287a18cac3a11ea5b17f1a992c6e1ad43ada
haden <git@haden.io>
2023-08-17 07:54:30 -0700

    Adds last 3 years or so of changes

--- home_zshrc.sh
+++ home_zshrc.sh
@@ -1,4 +1,40 @@
+alias rs="rsync -aHP --info=progress2 --info=stats3"
 alias c="clear && clear"
-alias e="exit"
-alias update="sudo apt update && apt list --upgradable && sudo apt upgrade"
+alias u="sudo apt update && apt list --upgradable && sudo apt upgrade"
 alias py="python3"
+alias rmd="rm -rf"
+alias t="tree -ah -I '.git' -I '*_venv'"
+alias dfj="df -hx 'squashfs'"
+alias dus="du -bhd 1 | sort -h"
+alias bat="batcat --paging never"
+
+# dstat
+alias dst="dstat -am --swap"
+
+# venv
+alias create_venv="~/dotfiles/create_venv.sh"
+alias run_in_venv="~/dotfiles/run_in_venv.sh"
+
+# tmux
+alias tl="tmux list-sessions"
+alias ta="tmux attach"
+alias tat="tmux attach -t"
+
+# toggle tmux mouse mode
+tm() {
+    if tmux show -g | grep -q "mouse on"; then
+        tmux set -g mouse off
+        echo "Tmux mouse mode off"
+    else
+        tmux set -g mouse on
+        echo "Tmux mouse mode on"
+    fi
+}
+
+# set nano as default editor
+export EDITOR="nano"
+
+# check if dotfiles-private exists and source it if it does
+if [ -f ~/dotfiles/dotfiles-private/extra_zshrc.sh ]; then
+    source ~/dotfiles/dotfiles-private/extra_zshrc.sh
+fi
